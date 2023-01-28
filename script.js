fb8e27d16b5ba80f3ddf22c00daa8f774a773e4c
muratdrmz <mrtdrmz@windowslive.com>
2023-01-28 19:00:37 +0100

    end

---
+++ script.js
@@ -0,0 +1,32 @@
+const num1=Math.round(Math.random()*10);
+const num2=Math.round(Math.random()*10);
+
+const correctAns=num1*num2;
+
+const formEl=document.getElementById('form');
+const scoreEl=document.getElementById('score');
+const questionEl=document.getElementById('question');
+const inputEl=document.getElementById('input');
+
+let score=JSON.parse(localStorage.getItem('score'))
+if(!score){
+ score=0;
+}
+
+scoreEl.innerText=`score:${score}`
+questionEl.innerText=`What is ${num1} multiply by ${num2}`
+
+formEl.addEventListener('submit',()=>{
+const userAnswer=+inputEl.value;
+if(userAnswer===correctAns){
+ score++;
+ updateLocalS()
+}else{
+ score--;
+ updateLocalS()
+}
+});
+
+function updateLocalS(){
+ localStorage.setItem('score',JSON.stringify(score))
+}
